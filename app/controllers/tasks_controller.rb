3d816b9176555853f6e6fcfee68a1babbbc49624
LEMB <ktaka.user@gmail.com>
2013-01-14 21:02:45 +0900

    add: hinagiku app

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,8 @@
+class TasksController < ApplicationController
+  def index
+    @page = 1
+    @index = 1
+
+    raise
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hinagiku::Application.config.secret_token = 'b88aade5a63042298f38e2f3009e5191d1831ab6a8c9a89641709757b37c8a8d9a2a416a61df6ba6c98577218f66d53e5c08e0627d8a23c76f287f7474d867dc'
